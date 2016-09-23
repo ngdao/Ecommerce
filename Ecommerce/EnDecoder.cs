e329581f494e658cca19cc856310c5ed48418f0f
Kyle Ferguson <hanbrolo117@gmail.com>
2016-09-22 18:44:43 -0700

    Implemented the EnDecode function which has static methods that can encode OrderObjects, as well as decode encoded Orderobject strings. Also implemented the OrderObject with which the EnDecoder class operates on.

---
+++ Ecommerce/EnDecoder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce
+{
+    class EnDecoder
+    {
+
+        /// <summary>
+        /// This function takes in a string which has an OrderOjbect "encoded" into it, and decodes it back into an OrderObject.
+        /// </summary>
+        /// <param name="orderStr">The "encoded" OrderObject string to decode into an OrderObject.</param>
+        /// <returns>The OderObject that was "decoded" from the "encoded" data passed in.</returns>
+        public static OrderObject Decode(String orderStr) {
+            //"decode" the "encoded" data:
+            string[] orderData = orderStr.Split(' ');
+
+            //Create a new orderObject to set up with the "decoded" data:
+            OrderObject orderObj = new OrderObject();
+
+            //Set up orderObject based on data in the encoded string:
+            orderObj.setSenderID(orderData[0]);                         //Set the Sender ID
+            orderObj.setReceiverID(orderData[1]);                       //Set the receiver ID
+            orderObj.setCardNo(Convert.ToInt32(orderData[2]));          //Set the Card Number
+            orderObj.setAmount(Convert.ToDecimal(orderData[3]));        //Set the Amount
+            orderObj.setUnitPrice(Convert.ToDecimal(orderData[4]));     //Set the Unit Price
+
+            //Return the newly orderObject that has been created from the "decoded" data:
+            return orderObj;
+        }
+
+        /// <summary>
+        /// This function takes in an OrderObject and "encodes" it into a string format.
+        /// </summary>
+        /// <param name="orderObj">The OrderObject to encode into a string.</param>
+        /// <returns>The "encoded" string representation of the OrderObject passed in.</returns>
+        public static String Encode(OrderObject orderObj) {
+            //Generate the new string to be the "encoded" orderObject:
+            string orderObj_toString = orderObj.getSenderID();
+            orderObj_toString += (" " + orderObj.getReceiverID());
+            orderObj_toString += (" " + orderObj.getCardNo());
+            orderObj_toString += (" " + orderObj.getAmount());
+            orderObj_toString += (" " + orderObj.getUnitPrice());
+
+            //Return the encoded String of the orderObject passed in:
+            return orderObj_toString;
+        }
+
+    }
+}
---
+++ Ecommerce/OrderObject.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce
+{
+    class OrderObject
+    {
+
+        private String senderID;
+        private String receiverID;
+        private int cardNo;
+        private decimal amount;
+        private decimal unitPrice;
+
+
+        public void setSenderID(string senderID)
+        {
+            this.senderID = senderID;
+        }
+
+        public void setReceiverID(string receiverID)
+        {
+            this.receiverID = receiverID;
+        }
+
+        public void setCardNo(int cardNo)
+        {
+            this.cardNo = cardNo;
+        }
+
+        public void setAmount(decimal amount)
+        {
+            this.amount = amount;
+        }
+
+        public void setUnitPrice(decimal unitPrice)
+        {
+            this.unitPrice = unitPrice;
+        }
+
+        public string getSenderID()
+        {
+            return this.senderID;
+        }
+
+        public string getReceiverID()
+        {
+            return this.receiverID;
+        }
+
+        public int getCardNo()
+        {
+            return this.cardNo;
+        }
+
+        public decimal getAmount()
+        {
+            return this.amount;
+        }
+
+        public decimal getUnitPrice()
+        {
+            return this.unitPrice;
+        }
+
+    }
+}
