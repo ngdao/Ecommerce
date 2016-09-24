faa83b15643255af429632586c431b464e1b93de
hehe876 <ndao3@asu.edu>
2016-09-23 23:30:52 -0700

    Data structure of MultiCellBuffer

--- Ecommerce/MultiCellBuffer.cs
+++ Ecommerce/MultiCellBuffer.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Ecommerce
 {
+    public class Cell
+    {
+        string encodedStr;
+
+        public Cell()
+        {
+            this.encodedStr = "";
+        }
+
+        public string getCell()
+        {
+            return this.encodedStr;
+        }
+
+        public void setCell(string message)
+        {
+            this.encodedStr = message;
+        }
+    }
     class MultiCellBuffer
     {
+        Semaphore pool = new Semaphore(0,3);
+        Cell[] cellArray;
+
+        public MultiCellBuffer()
+        {
+            for (int i = 0; i < 3; i++)
+                this.cellArray[i] = new Cell();
+        }
+
+        //Cell getOneCell() { return Cell someCell; }
+        //void setOneCell() { cellArray[someCell].encodedStr = someString;}
     }
+
 }
